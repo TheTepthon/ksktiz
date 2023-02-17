c884ce66cc7f4513f3738685fcaac30b1099a456
Source Tepthon <anes12salih@gmail.com>
2023-02-17 08:20:37 +0300

    Update queues.py

--- MusicUserbot/helpers/queues.py
+++ MusicUserbot/helpers/queues.py
@@ -1,31 +1,29 @@
 QUEUE = {}
 
-
 def add_to_queue(chat_id, songname, link, ref, type, quality):
     if chat_id in QUEUE:
         chat_queue = QUEUE[chat_id]
         chat_queue.append([songname, link, ref, type, quality])
-        return int(len(chat_queue) - 1)
+        QUEUE[chat_id] = chat_queue
+        return len(chat_queue) - 1
     else:
         QUEUE[chat_id] = [[songname, link, ref, type, quality]]
-
+        return 0
 
 def get_queue(chat_id):
     if chat_id in QUEUE:
-        chat_queue = QUEUE[chat_id]
-        return chat_queue
+        return QUEUE[chat_id]
     else:
-        return 0
-
+        return []
 
 def pop_an_item(chat_id):
     if chat_id in QUEUE:
         chat_queue = QUEUE[chat_id]
         chat_queue.pop(0)
+        QUEUE[chat_id] = chat_queue
         return 1
     else:
         return 0
-
 
 def clear_queue(chat_id):
     if chat_id in QUEUE:
